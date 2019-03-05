3b3b1613e3548b77114b70369925ef9a7b14d1db
Ashish Garg <mragarg@gmail.com>
2019-03-05 10:50:12 -0500

    Adds leetspeak dictionary in JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-// Save
+console.log("Hello");
+
+const leetDictionary = {
+    // Keys are strings, just 
+
+    // But in JS, quotes are optional 
+    A: 4,
+    E: 3,
+    G: 6,
+    I: 1,
+    O: 0,
+    S: 5,
+    T: 7
+};
