0055d5802934dc21c85163c7fcafae82df613b06
Ashish Garg <mragarg@gmail.com>
2019-03-05 11:55:24 -0500

    Adds addressbook, function to print all entries, function to print one entry's information

--- scripts/index.js
+++ scripts/index.js
@@ -31,5 +31,55 @@ function transl33t(originalString){
 }
 
 const addressBook = {
+    DigitalCrafts: {
+        phone: "404-123-1234",
+        address: "3423 Piedmont Rd",
+        city: "Atlanta"
+    },
+    "Chris Aquino": {
+        phone: "404-456-1234",
+        address: "3400 Piedmont Rd",
+        city: "Atlanta"
+    },
+    "Ashish Garg": {
+        phone: "404-222-2222",
+        address: "1234 10th Street",
+        city: "Atlanta"
+    },
+    "Jon Snow": {
+        phone: "000-000-0000",
+        address: "1 Winterfell Lane",
+        city: "GoT"
+    }
+}
+
+// Used to access information within a Dictionary
+addressBook.DigitalCrafts.address;
+addressBook["Chris Aquino"].phone;
+addressBook["Chris Aquino"]["phone"];
+
+// Used to get the keys of a dictionary in the form of an array of strings
+Object.keys(addressBook);
+Object.keys(addressBook.DigitalCrafts);
+
+// write a function that accepts an address book and prints all of the entries
+function printBook(book){
+    // Array with the keys (person name)
+    let personKeys = Object.keys(book);
     
+    // Loop goes through each person
+    for(let i = 0; i < personKeys.length; i++) {
+        console.log(personKeys[i]);
+        console.log("Phone: " + book[personKeys[i]].phone);
+        console.log("Address: " + book[personKeys[i]].address);
+        console.log("City: " + book[personKeys[i]].city);
+    }
+}
+
+// write a second function that accepts an address book and a key name, and prints only that piece of contact info
+function printBookPerson(book, key){
+    console.log(key);
+    console.log("Phone: " + book[key].phone);
+    console.log("Address: " + book[key].address);
+    console.log("City: " + book[key].city);
 }
